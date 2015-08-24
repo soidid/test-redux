798c3adc6a07af632650b61f441ec228aad78770
soidid <soidid@gmail.com>
2015-08-24 16:06:42 +0800

    change webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: [
-    
-    './main.js'
-  ],
+  entry:'./main.js',
   output: {
     //path: path.join(__dirname, 'static'),
     path: './static',
